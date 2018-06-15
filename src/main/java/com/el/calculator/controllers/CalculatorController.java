595a128791d183d070814e28a42455be24e7e616
Evan Bradley <evan.bradley@ally.com>
2018-06-15 13:43:34 -0400

    Added skeleton for controller.

--- src/main/java/com/el/calculator/controllers/CalculatorController.java
+++ src/main/java/com/el/calculator/controllers/CalculatorController.java
@@ -1,5 +1,35 @@
 package com.el.calculator.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.el.calculator.models.Calculator;
+
+@Controller
+@RequestMapping("/")
 public class CalculatorController {
+
+    private Calculator calculator;
+
+    public CalculatorController() {
+        calculator = new Calculator();
+    }
+
+    @GetMapping("")
+    public String showDefaultPage() {
+        return "default";
+    }
+
+    @PostMapping("")
+    public ModelAndView convertNumber(double firstNumber, double secondNumber, String operation) {
+        String result = "";// = calculator.calc(operation, firstNumber, secondNumber);
+
+        ModelAndView mv = new ModelAndView("romanNumerals/default");
+        mv.addObject("result", result);
+        return mv;
+    }
 
 }
