dcff1f936ae18af91ff812e367f6f878cd74ae7d
FZMWDN@ALLYIPOC.COM <FZMWDN@ALLYIPOC.COM>
2018-06-15 15:46:16 -0400

    attempted to connect csv to Junit tests

--- src/test/java/com/el/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/el/calculator/CalculatorApplicationTests.java
@@ -2,18 +2,14 @@ package com.el.calculator;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized.Parameters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.el.calculator.models.Calculator;
-
-import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,10 +33,21 @@ public class CalculatorApplicationTests {
 //        this.operator = operator;
 //        this.expectedOutput = expectedOutput;
 //    }
-
+    
+  @ParameterizedTest(name = "{index} => op={0}, first={1}, second={2}, expected= {3}")
+	@CsvFileSource(resources = "/test_cases.csv")
+	void sum(String op, double first, double second, String expected) {
+	  System.out.println("hello");
+	  System.out.println(expected);
+	  String actual = Calculator.calculate(op, first, second);
+	  assertEquals(expected,actual);
+  }
+ 
+    
 	@Test
 	public void contextLoads() {
         assertEquals(Calculator.calculate(operator,a,b),expectedOutput);
+        System.out.println("hello");
 	}
 
 }
